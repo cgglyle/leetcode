aa432b64dff5d9d06aa03c312f27c2871eff2888
Lyle Liu <i@cgglyle.top>
2024-02-27 11:38:29 +0800

    add spock test

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("groovy")
 }
 
 group = "top.cgglyle.leetcode"
@@ -28,6 +29,8 @@ dependencies {
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// https://mvnrepository.com/artifact/org.spockframework/spock-core
+	testImplementation("org.spockframework:spock-core:2.4-M1-groovy-4.0")
 }
 
 tasks.withType<Test> {
