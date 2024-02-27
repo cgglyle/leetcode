4455074523b0498b38f7b8a2018add52dc36e8f8
Lyle Liu <i@cgglyle.top>
2024-02-27 10:26:16 +0800

    roman to integer best

--- src/main/java/top/cgglyle/leetcode/RomanToInteger.java
+++ src/main/java/top/cgglyle/leetcode/RomanToInteger.java
@@ -1,47 +1,47 @@
 package top.cgglyle.leetcode;
 
-import lombok.val;
-
-import java.util.HashMap;
-
 public class RomanToInteger {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> romanMap = new HashMap<>();
-        romanMap.put('I', 1);
-        romanMap.put('V', 5);
-        romanMap.put('X', 10);
-        romanMap.put('L', 50);
-        romanMap.put('C', 100);
-        romanMap.put('D', 500);
-        romanMap.put('M', 1000);
-
         char[] charArray = s.toCharArray();
         int reslut = 0;
         for (int i = 0; i < charArray.length; i++) {
             char c = charArray[i];
-            int number = romanMap.get(c);
-            if (i + 1 < charArray.length) {
-                char nextC = charArray[i + 1];
-                switch (c) {
-                    case 'I' -> {
-                        if (nextC == 'V' || nextC == 'X') {
-                            number = -romanMap.get(c);
-                        }
-                    }
-                    case 'X' -> {
-                        if (nextC == 'L' || nextC == 'C') {
-                            number = -romanMap.get(c);
-                        }
-                    }
-                    case 'C' -> {
-                        if (nextC == 'D' || nextC == 'M') {
-                            number = -romanMap.get(c);
-                        }
-                    }
-                }
+            int number = getValue(c);
+            if (i + 1 < charArray.length && number < getValue(charArray[i + 1])) {
+                reslut -= number;
+            } else {
+                reslut += number;
             }
-            reslut += number;
         }
         return reslut;
+    }
+
+    private int getValue(char c) {
+        switch (c) {
+            case 'I' -> {
+                return 1;
+            }
+            case 'V' -> {
+                return 5;
+            }
+            case 'X' -> {
+                return 10;
+            }
+            case 'L' -> {
+                return 50;
+            }
+            case 'C' -> {
+                return 100;
+            }
+            case 'D' -> {
+                return 500;
+            }
+            case 'M' -> {
+                return 1000;
+            }
+            default -> {
+                return 0;
+            }
+        }
     }
 }
